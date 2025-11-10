09a4b4e382a8552718eed7447fd8ce2e1e2e050c
mardenfilhonetinho-ai <mardenfilho.netinho@gmail.com>
2025-11-10 11:17:14 -0400

    alugames-teste

---
+++ alugames/js/app.js
@@ -0,0 +1,16 @@
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+
+    if (botao.textContent === 'Devolver') {
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.textContent = 'Alugar';
+        botao.classList.remove('dashboard__item__button--return');
+    } else {
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return');
+
+    }
+}
